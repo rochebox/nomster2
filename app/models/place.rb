ca9274e63db197a0585a70a6cad7d9e9d5d8e640
rochebox <rochebox@gmail.com>
2016-06-29 18:30:48 +0000

    Validations for address, description and lenght of restaurant name Lesson 28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,10 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, presence: true
+	# used to be --> validates :name, presence: true
+	validates :name, :address, :description, presence: true
+	validates :name, length: { minimum: 3, :allow_blank => true , too_short: "Name must have at least 3 characters!" }
+
+    # alternative
+    #validates :name, presence: true, length: { minimum: 3, :allow_blank => true , too_short: "Name must have at least 3 characters!" }
+    #validates :address, :description, presence: true
 end
