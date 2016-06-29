4beda335354219a9920b8119b08edbe200f309be
rochebox <rochebox@gmail.com>
2016-06-29 20:28:27 +0000

    Adding Geocode and Bing Account functionality to project

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	geocoded_by :address
+  	after_validation :geocode
+  	
 	# used to be --> validates :name, presence: true
 	validates :name, :address, :description, presence: true
 	validates :name, length: { minimum: 3, :allow_blank => true , too_short: "Name must have at least 3 characters!" }
