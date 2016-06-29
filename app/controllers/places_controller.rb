71e69a013e09207bfe84f1560671f4516c75ac1b
rochebox <rochebox@gmail.com>
2016-06-29 15:31:24 +0000

    Adding Destroy-Delete permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
 		# see https://github.com/mislav/will_paginate
@@ -37,13 +37,16 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render text: 'Not Allowed, Dude. Not Cool.', status: :forbidden
 		end
-		
+
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
 
 	def destroy
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render text: "Not Allowed. Can't do this", status: :forbidden
+		end
 		@place.destroy
 		redirect_to root_path
 	end
